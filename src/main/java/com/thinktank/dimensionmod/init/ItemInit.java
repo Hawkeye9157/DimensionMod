841ee455b4c85ff47d872b5ad097f0f09a45df98
Hawkeye9157 <157405016+Hawkeye9157@users.noreply.github.com>
2024-11-06 11:54:21 -0700

    Mod Setup

---
+++ src/main/java/com/thinktank/dimensionmod/init/ItemInit.java
@@ -0,0 +1,21 @@
+/**
+ * @author csipes
+ * @createdOn 11/5/2024 at 8:36 PM
+ * @projectName DimensionMod
+ * @packageName com.thinktank.dimensionmod.init;
+ */
+package com.thinktank.dimensionmod.init;
+
+import com.thinktank.dimensionmod.DimensionMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+//class of added items
+public class ItemInit {
+    
+}
+
